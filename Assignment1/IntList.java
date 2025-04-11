e946a907c38ace8d45e7348d30dea48a2f045114
jan-schw <61502392+jan-schw@users.noreply.github.com>
2025-04-11 21:18:00 +0200

    add basic layout for task 2

---
+++ Assignment1/IntList.java
@@ -0,0 +1,26 @@
+public class IntList {
+    
+    public static void main(String[] args) {
+        
+    }
+
+    //a
+    public static int countNumbers(int num){
+        return 0;
+    }
+
+    //b
+    public static int[] intToList(int value){
+        return null;
+    }
+
+    //c
+    public static int listToInt(int[] numbers){
+        return 0;
+    }
+
+    //d
+    public static int[] addOne(int[] numbers){
+        return null;
+    }
+}
