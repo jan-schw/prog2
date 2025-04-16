c3eb0586d9ab41dc56b93da04bf2cf970bbab605
jan-schw <61502392+jan-schw@users.noreply.github.com>
2025-04-16 19:20:01 +0200

    add test cases

--- Assignment2/Main.java
+++ Assignment2/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
 
     public static void main(String[] args) {
+        //Baum aus Figur 1 aufbauen
         Tree five = new Tree(5);
         Tree two = new Tree(2);
         Tree minusfour = new Tree(-4);
@@ -12,17 +13,28 @@ public class Main {
 
         five.setRight(eighteen);
         five.setLeft(two);
-
         two.setLeft(minusfour);
         two.setRight(three);
 
-        String order = five.str();
-        System.out.println(order);
+        System.out.println(five.str());
 
+        //Neue Werte hinzufügen
+        five.insertValue(29);   //rechts von der 18
+        System.out.println("Contains 29? " + five.contains(29));
+        five.insertValue(21);   //links von der 18
+        System.out.println("Contains 21? " + five.contains(21));
+        five.insertValue(1);    //rechts von der -4
+        System.out.println("Contains 1? " + five.contains(1));
+        System.out.println(five.str());
 
-        boolean contains = five.contains(1);
-        System.out.println(contains);
+        //Einige Werte löschen
+        five.deleteValue(-4);   //1 soll jetzt links an der 2 hängen
+        System.out.println("Contains -4? " + five.contains(-4));
+        five.deleteValue(2);   //1 soll jetzt links an der 3 hängen
+        System.out.println("Contains 2? " + five.contains(2));
+        five.deleteValue(18);   //29 soll jetzt rechts von der 5 hängen, links von der 29 dann die 21
+        System.out.println("Contains 18? " + five.contains(18));
 
-        five.insertValue(4);
+        System.out.println(five.str());
     }
 }
