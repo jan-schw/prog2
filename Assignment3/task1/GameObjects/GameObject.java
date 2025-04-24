9b6855e381ce0bfaabf43158aea6a9eb83dc7ce1
jan-schw <61502392+jan-schw@users.noreply.github.com>
2025-04-24 21:54:57 +0200

    add more javadoc comments to further reduce visibility

--- Assignment3/task1/GameObjects/GameObject.java
+++ Assignment3/task1/GameObjects/GameObject.java
@@ -1,18 +1,37 @@
 package GameObjects;
 
+/**
+ * Different ingame objects for the GameView class
+ * Includes five different objects with corresponding symbol in the game 
+ * 
+ * @author Jan Schweers
+ * @version 24.04.2025
+ */
 public enum GameObject {
+
+    /**  The five different objects that can be represented by this enum in the game */
     GROUND('G'),
     LAVA('L'),
     AIROBSTACLE('O'),
     EMPTY(' '),
     GOAL('Z');
 
+    /** Symbol of the current object */
     private char symbol;
 
+    /**
+     * Constructor for GameObject
+     * @param symbol Symbol of the current game object
+     */
     private GameObject(char symbol){
         this.symbol = symbol;
     }
 
+    /**
+     * Getter for symbol
+     * {@link GameObjects.GameObject.symbol} is the symbol of the current object
+     * @return symbol
+     */
     public char getSymbol(){ 
         return this.symbol;
     }
