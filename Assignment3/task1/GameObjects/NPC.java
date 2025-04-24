1b30a5725055443637649c739d6d2c5c6a63f753
jan-schw <61502392+jan-schw@users.noreply.github.com>
2025-04-24 21:51:34 +0200

    add javadoc comments to npc class to reduce visibility

--- Assignment3/task1/GameObjects/NPC.java
+++ Assignment3/task1/GameObjects/NPC.java
@@ -1,36 +1,75 @@
 package GameObjects;
 
+/**
+ * Non-playable-character for the GameView class
+ * Tracks if he is crouching or standing up and the current x coordinate
+ * 
+ * @author Jan Schweers
+ * @version 24.04.2025
+ */
 public class NPC {
+    /** State if npc is currently crouching or not */
     private boolean isCrouching;
+    /** Current x coordinate of the npc in the game */
     private int xCoordinate;
 
+    /**
+     * Constructor for NPC class
+     * @author Jan Schweers
+     */
     public NPC(){
         this.isCrouching = false;
         this.xCoordinate = 0;
     }
 
+    /**
+     * Let's the NPC stand up
+     * @author Jan Schweers
+     */
     public void standUp(){
         this.isCrouching = false;
     }
 
+    /**
+     * Let's the NPC crouch
+     * @author Jan Schweers
+     */
     public void crouch(){
         this.isCrouching = true;
     }
 
+    /**
+     * Lets the NPC walk one field to the right
+     * @author Jan Schweers
+     */
     public void walkRight(){
         this.xCoordinate++;
     }
 
+    /**
+     * Lets the NPC dash two fields to the right
+     * @author Jan Schweers
+     */
     public void dashRight(){
         if(!isCrouching){
             this.xCoordinate += 2;
         }
     }
 
+    /**
+     * Returns value of isCrouching
+     * {@link GameObjects.NPC.isCrouching} State if npc is currently crouching or not
+     * @return isCrouching
+     */
     public boolean isCrouching() {
         return isCrouching;
     }
 
+    /**
+     * Getter for xCoordinate
+     * {@link GameObjects.NPC.xCoordinate} Current x coordinate of the npc in the game
+     * @return xCoordinate
+     */
     public int getxCoordinate() {
         return xCoordinate;
     }
