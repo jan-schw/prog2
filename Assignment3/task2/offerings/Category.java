197eb882d7183159be29d34f60574902a5812301
jan-schw <61502392+jan-schw@users.noreply.github.com>
2025-04-24 23:08:47 +0200

    add javadoc comments to category class

--- Assignment3/task2/offerings/Category.java
+++ Assignment3/task2/offerings/Category.java
@@ -1,16 +1,32 @@
 package task2.offerings;
 
+/**
+ * The class represents categories for the items in the marketplace
+ * Multiple different categories with the corresponding string representation
+ * @author Jan Schweers
+ * @version 24.04.2025
+ */
 public enum Category {
+    /** Represents the different categories items can be sorted into */
     FURNITURE("Furniture"),
     ELECTRONICS("Electronics"),
     OTHER("Other");
 
+    /** String representation of the current category */
     private String stringRepresentation;
 
+    /**
+     * Constructor for Category
+     * @param rep string representation of the current category
+     */
     private Category(String rep){
         this.stringRepresentation = rep;
     }
 
+    /**
+     * Returns string representation of current category
+     * @return string representation
+     */
     public String str(){
         return this.stringRepresentation;
     }
