f450aa599d2dcbcb940b7a6c9028d670f285acd2
jan-schw <61502392+jan-schw@users.noreply.github.com>
2025-04-24 20:37:16 +0200

    add code for task b

--- Assignment3/task1/GameObjects/NPC.java
+++ Assignment3/task1/GameObjects/NPC.java
@@ -1,5 +1,19 @@
 package GameObjects;
 
 public class NPC {
-    
+    private boolean isCrouching;
+    private int xCoordinate;
+
+    public NPC(){
+        this.isCrouching = false;
+        this.xCoordinate = 0;
+    }
+
+    public void standUp(){
+        this.isCrouching = false;
+    }
+
+    public void crouch(){
+        this.isCrouching = true;
+    }
 }
