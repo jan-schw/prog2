3a70458aea7c4776f13fc2b04c793e5c01b4c36b
jan-schw <61502392+jan-schw@users.noreply.github.com>
2025-04-25 08:28:25 +0200

    add code for debug task

--- Assignment3/Debug.java
+++ Assignment3/Debug.java
@@ -15,13 +15,13 @@ class Expression {
 
   double evaluate() {
     switch (this.op_) {
-      case Operator.ADD:
+      case ADD:             //Operator.ADD zu ADD geändert. Hier wird nicht der Typ benötigt, sondern nur der Wert des enums
         return this.left_ + this.right_;
-      case Operator.SUBTRACT:
+      case SUBTRACT:        //Operator.SUBTRACT zu SUBTRACT geändert. Grund: s.o.
         return this.left_ - this.right_;
-      case Operator.MULTIPLY:
+      case MULTIPLY:        //Operator.MULTIPLY zu MULTIPLY geändert. Grund: s.o.
         return this.left_ * this.right_;
-      case Operator.DIVIDE:
+      case DIVIDE:          //Operator.DIVIDE zu DIVIDE geändert. Grund: s.o.
         return this.left_ / this.right_;
       default:
         return 0.0;
@@ -32,7 +32,7 @@ class Expression {
 class Debug {
 
   public static void main(String[] args) {
-    Operator[] ops = new Operator[5];
+    Operator[] ops = new Operator[4];   //Array-Größe auf 4 reduziert, das reicht für die Operatoren und führt nicht zu NullPointern
     ops[0] = Operator.DIVIDE;
     ops[1] = Operator.SUBTRACT;
     ops[2] = Operator.MULTIPLY;
@@ -40,7 +40,7 @@ class Debug {
 
     Expression[] exp = new Expression[ops.length];
     for (int i = 0; i < ops.length; ++i) {
-      exp[i] = new Expression(i + 1, i, ops[i]);
+      exp[i] = new Expression(i, i + 1, ops[i]);  //i und i+1 getauscht, damit es nicht zu einer Division durch 0 kommt
     }
 
     for (int i = 0; i < ops.length; ++i) {
