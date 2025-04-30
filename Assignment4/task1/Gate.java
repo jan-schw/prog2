17cc904508dcdb9b649fe0873cac247f0a023159
jan-schw <61502392+jan-schw@users.noreply.github.com>
2025-04-30 17:04:39 +0200

    add code for 1a

---
+++ Assignment4/task1/Gate.java
@@ -0,0 +1,35 @@
+package task1;
+
+public class Gate {
+
+    private String symbol;
+
+    public boolean evaluate(boolean a, boolean b) {
+        return false;
+    }
+
+    public void table() {
+        String finalPrint = "";
+        String header = String.format(" A | B | Y=A%sB \n", this.symbol);
+        String separator = "___|___|___\n";
+        finalPrint += header + separator;
+
+        for (int i = 0; i < 2; i++) {
+
+            for (int y = 0; y < 2; y++) {
+                boolean rowA = i < 1 ? false : true;
+                boolean rowB = y == 0 ? false : true;
+                boolean rowResult = this.evaluate(rowA, rowB);
+                String line = String.format(" %s | %s | %s \n",
+                        this.boolToString(rowA), this.boolToString(rowB), this.boolToString(rowResult));
+                finalPrint += line + separator;
+            }
+
+        }
+        System.out.println(finalPrint);
+    }
+
+    private String boolToString(boolean input) {
+        return input ? "1" : "0";
+    }
+}
